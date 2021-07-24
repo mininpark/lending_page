1b1ee6bfb30e4d7b5b2c10338602296724c1a874
Min Park <61059800+mininpark@users.noreply.github.com>
2021-07-24 13:46:50 +0200

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,7 @@
+var box = document.querySelector('.prog_box');
+var width = document.getElementById('.prog_box').offsetWidth
+
+window.onload = function(){
+    var box_size = document.getElementById(h3);
+    box_size.style.width = 70 + "%"
+}
